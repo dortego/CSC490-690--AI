35606d6522ad88a27538be2dc0c1e99806fff2da
dortego <daveortego1988@gmail.com>
2018-10-01 15:40:49 -0500

    very basic commit

--- src/ai_game/Trapdoor.java
+++ src/ai_game/Trapdoor.java
@@ -5,6 +5,7 @@
  */
 package ai_game;
 
+import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
@@ -14,11 +15,17 @@ import javax.swing.JLabel;
  */
 public class Trapdoor extends JLabel {
   
+  Dimension d = new Dimension(2, 2);
   ImageIcon trapdoor = new ImageIcon(getClass().getResource("Trap_Door_open.png"));  
-  JLabel trapdoorLabel = new JLabel(trapdoor, JLabel.CENTER).setLocation(350, 350);
+//  JLabel trapdoorLabel = new JLabel(trapdoor, JLabel.CENTER).setLocation(350, 350);
   
   public Trapdoor() {
-    
+//    trapdoor.set
+    this.setIcon(trapdoor);
+    this.setSize(d);
+    this.setMinimumSize(d);
+    this.setPreferredSize(d);
+    this.setMinimumSize(d);
   }
   
 }
